dc15d68404f6355f66bc8901999d0ee539e19744
OzerOzturk <ozerozturk1636@gmail.com>
2022-01-11 23:57:28 +0300

    created react app and started to use react mapbox.

---
+++ frontend/src/App.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import ReactMapGL from "react-map-gl";
+
+function App() {
+  const [viewport, setViewport] = useState({
+    width: "100vw",
+    height: "100vh",
+    latitude: 46,
+    longitude: 17,
+    zoom: 4,
+  });
+
+  return (
+    <div className="App">
+      <ReactMapGL
+        {...viewport}
+        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX}
+        onViewportChange={(nextViewport) => setViewport(nextViewport)}
+      />
+    </div>
+  );
+}
+
+export default App;
