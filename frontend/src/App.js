066cda20c1396f6b1ab3b6e783d6f1f0495b7a0c
OzerOzturk <ozerozturk1636@gmail.com>
2022-01-13 22:48:32 +0300

    mapbox marker and mapbox popup were created.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import { useState } from "react";
-import ReactMapGL from "react-map-gl";
+import ReactMapGL, { Marker, Popup } from "react-map-gl";
+import { Room, Star } from "@material-ui/icons";
+import "./app.css";
 
 function App() {
   const [viewport, setViewport] = useState({
@@ -16,7 +18,44 @@ function App() {
         {...viewport}
         mapboxApiAccessToken={process.env.REACT_APP_MAPBOX}
         onViewportChange={(nextViewport) => setViewport(nextViewport)}
-      />
+        mapStyle="mapbox://styles/safak/cknndpyfq268f17p53nmpwira"
+      >
+        <Marker
+          latitude={52.516266}
+          longitude={13.377775}
+          offsetLeft={-20}
+          offsetTop={-10}
+        >
+          <Room style={{ fontSize: viewport.zoom * 6, color: "stateblue" }} />
+        </Marker>
+        <Popup
+          latitude={52.516266}
+          longitude={13.377775}
+          closeButton={true}
+          closeOnClick={false}
+          anchor="bottom"
+        >
+          <div className="card">
+            <label>Place</label>
+            <h4 className="place">Brandenburg Gate</h4>
+            <label>Review</label>
+            <p className="desc">What a historical place.</p>
+            <label>Rating</label>
+            <div className="stars">
+              <Star className="star" />
+              <Star className="star" />
+              <Star className="star" />
+              <Star className="star" />
+              <Star className="star" />
+            </div>
+            <label>Information</label>
+            <span className="username">
+              Created by <b>Özer</b>
+            </span>
+            <span className="date">1 hour ago </span>
+          </div>
+        </Popup>
+      </ReactMapGL>
     </div>
   );
 }
